2cd6b4d61b20d4df123161e9aa0cb7b92e4fd041
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-08-13 14:11:31 +0200

    typescript error correction

--- src/App.tsx
+++ src/App.tsx
@@ -18,11 +18,12 @@ interface Task {
       setTasks(storedTasks);
     }, []);
   
-    const handleSubmit = (e: React.FormEvent) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      const titleInput = e.currentTarget['title'] as HTMLInputElement;
-      const descriptionInput = e.currentTarget['description'] as HTMLTextAreaElement;
-  
+      const target = e.currentTarget;
+      const titleInput = target['title'] as HTMLInputElement;
+      const descriptionInput = target['description'] as HTMLTextAreaElement;
+      
       const newTask: Task = {
         title: titleInput.value,
         description: descriptionInput.value,
@@ -54,8 +55,8 @@ interface Task {
         <form onSubmit={handleSubmit} className="bg-slate-600 p-10 rounded-md max-w-md">
             <label for="title" className="block">Title</label>
             <input className="bg-gray-700 px-1 py-2 w-full rounded-md mb-2" name="title" type="text" placeholder="write a title"/>
-            <label for="tdescription" className="block">Description</label>
-            <textarea className="bg-gray-700 px-1 py-2 w-full rounded-md mb-2" name="description" rows="3"></textarea>
+            <label htmlFor="tdescription" className="block">Description</label>
+            <textarea className="bg-gray-700 px-1 py-2 w-full rounded-md mb-2" name="description" rows={3}></textarea>
             <button className="bg-blue-600 px-3 py-1 rounded-md hover:bg-blue-500">Save</button>
           </form>
           <div className="max-w-md w-full h-3/5 px-10 py-5 overflow-y-scroll rounded-md">
